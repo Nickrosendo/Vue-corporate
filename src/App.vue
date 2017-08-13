cd988565a6006c067bd58db90133f09c6098536f
Nickrosendo <nicolasrosendo@hotmail.com>
2017-08-12 21:07:52 -0300

    Menu Component

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,34 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Just+Another+Hand|Lato:300,400,700');
+
+  //
+  // Globally
+  //
+
+  html, body {
+    width: 100vw;
+    height: 100vh;
+    max-width:100%;
+    margin: 0;
+    padding: 0;
+    font-weight: 300;
+    font-size: 16px;
+  }
+  h1, h2, h3, h4, h5, h6,
+  .h1, .h2, .h3, .h4, .h5, .h6 { font-weight: 700; }
+  strong { font-weight: 500; }
+  .bold{ font-weight: 700; }
+  a{
+    color:  rgba(0, 0, 0, 0.5);
+    text-decoration: none !important;
+    &hover{
+      color: rgba(0, 0, 0, 0.7);
+      text-decoration: none !important;
+      cursor: pointer !important;
+    }
+  }
 
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,26 +2,46 @@
   <div class="octadeskMenu">
     <nav class="container ">
       <div id="od-main-nav" class="collapse navbar-collapse">
-        <a class="navbar-brand hidden-md-down">
+        <a class="octadeskMenu-logo hidden-md-down">
           <img src="../assets/images/logos/logo-octadesk.png" width="153" height="34" alt="Logo octadesk" title="Logo octadesk">
         </a>
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-			<li class="nav-item">
-				<a class="nav-link">Funcionalidades</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link">Cases</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link">Preços</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link">Materiais</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link">Contato</a>
-			</li>
-		</ul>
+        <ul class="octadeskMenu-nav">
+          <li class="octadeskMenu-nav-item">
+            <a >Funcionalidades</a>
+          </li>
+          <li class="octadeskMenu-nav-item">
+            <a>Cases</a>
+          </li>
+          <li class="octadeskMenu-nav-item">
+            <a>Preços</a>
+          </li>
+          <li class="octadeskMenu-nav-item">
+            <a>Materiais</a>
+          </li>
+          <li class="octadeskMenu-nav-item">
+            <a>Contato</a>
+          </li>
+        </ul>
+        <form class="octadeskMenu-form">
+          <fieldset>
+            <div class="input-group">
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Informe seu email"
+                required
+              >
+              <span class="input-group-btn">
+                <button
+                  type="submit"
+                  class="btn btn-success"
+                >
+                  Avalie grátis
+                </button>
+              </span>
+            </div>
+          </fieldset>
+        </form>
       </div>
     </nav>
   </div>
@@ -40,10 +60,40 @@ export default {
     top: 	0;
     left: 0;
     right: 0;
+    padding: .5rem 1rem;
     background-color: #fff;
+    color: rgba(0,0,0,.5);
     font-weight: 400;
     z-index: 1030;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12), 0 3px 1px -2px rgba(0,0,0,.2);
+  }
+
+  .octadeskMenu-logo{
+    display: inline-block;
+    padding-top: .25rem;
+    padding-bottom: .25rem;
+    margin-right: 1rem;
+    font-size: 1.25rem;
+    line-height: inherit;
+    white-space: nowrap;
+  }
+
+  .octadeskMenu-nav{
+    display: inline-block;
+    padding-left: 0;
+    margin-bottom: 0;
+    margin-right: auto;
+    list-style: none
+  }
+
+  .octadeskMenu-nav-item{
+    display: inline-block;
+  }
+
+  .octadeskMenu-form{
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
   }
 
 </style>
